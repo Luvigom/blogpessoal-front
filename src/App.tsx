8cedf46f96f7f56beaeea3fc73115a40f172b29e
Lucas Vinicius <lucas.vinicius@sumup.com>
2024-06-26 15:53:18 -0300

    Atividade React

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+
+import Login from './paginas/login/Login';
+import Home from './paginas/home/Home';
+import Cadastro from './paginas/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
+
+function App() {
+  return (
+    <>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
+    </>
+  );
+}
+export default App;
